769f3e530d9ae7ede3eca6dc18f6ce7a23449e42
Edu Benito <eduardbenito@icloud.com>
2018-09-20 17:46:48 +0200

    fix service worker problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,8 @@ import { environment } from '../environments/environment';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
